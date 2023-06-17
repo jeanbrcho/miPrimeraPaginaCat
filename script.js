c11ef39d03a92c100a5cadc8000314937adccbe9
jeanbrcho <jeancbs18@gmail.com>
2023-06-16 23:33:52 -0300

    repare menu responsive y agregue audio

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-const form = document.getElementById('myForm');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  validateForm();
-});
-function validateEmail(email) {
-    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
-    return regex.test(email)
-  }
-function validateForm() {
-  const emailInput = document.getElementById('email');
-  const email = emailInput.value;
-  if (!validateEmail(email)) {
-    alert('Por favor ingrese un correo electrónico válido.');
-  } else {
-    alert('Correo electrónico enviado correctamente.');
-  }
-}
-
 function showAlert() {
     alert("¡Hola, esta es una alerta desde JavaScript!");
 }
 
 document.querySelector("button.button-menu-toggle")
-
 .addEventListener("click", function() {
-document.querySelector(".nav-links").
-            classList.toggle("nav-links-responsive")})
+document.querySelector(".nav-links").classList.toggle("nav-links-responsive")});
+
+function mostrarOcultarAudio() {
+  let audio = document.getElementById("audio");
+  if (audio.style.visibility == "hidden") {
+    audio.style.visibility = "visible";
+  } else {
+    audio.style.visibility = "hidden";
+  }
+}
