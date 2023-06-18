f5fc32c9d38ecd9934681271ca3fdd5372005c89
jeanbrcho <jeancbs18@gmail.com>
2023-06-18 19:19:31 -0300

    agregue slider de imagenes

--- script.js
+++ script.js
@@ -14,3 +14,54 @@ function mostrarOcultarAudio() {
     audio.style.visibility = "hidden";
   }
 }
+
+
+// Carrusel
+const slider = document.querySelector('.slider');
+const prevBtn = document.querySelector('.slider-prev');
+const nextBtn = document.querySelector('.slider-next');
+let currentIndex = 0;
+let startX;
+let isDragging = false;
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slider.children.length) % slider.children.length;
+  updateSlider();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slider.children.length;
+  updateSlider();
+});
+
+slider.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startX = e.clientX;
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (isDragging) {
+    const currentX = e.clientX;
+    const diffX = currentX - startX;
+    const moveRatio = diffX / slider.offsetWidth;
+    const moveThreshold = 0.1;
+
+    if (Math.abs(moveRatio) >= moveThreshold) {
+      if (moveRatio < 0) {
+        currentIndex = (currentIndex + 1) % slider.children.length;
+      } else {
+        currentIndex = (currentIndex - 1 + slider.children.length) % slider.children.length;
+      }
+      updateSlider();
+      isDragging = false;
+    }
+  }
+});
+
+slider.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+function updateSlider() {
+  slider.style.transform = `translateX(-${currentIndex * 33.33}%)`;
+}
